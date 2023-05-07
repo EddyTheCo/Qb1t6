7a75754490a7dc8b09d284f1df8710691088ee19
eddyTheCo <eddyTheCo@esterVtech.com>
2023-05-07 08:46:56 +0000

    testing CI/CD

--- tests/tip5.cpp
+++ tests/tip5.cpp
@@ -1,5 +1,4 @@
 #include"encoding/qb1t6.hpp"
-#include<iostream>
 #undef NDEBUG
 #include <assert.h>
 
@@ -14,7 +13,7 @@ int main(int argc, char** argv)
 
     binary=QByteArray::fromHex("0001027e7f8081fdfeff");
     ternary=get_Trytes_from_Bytes(binary);
-    assert(ternary==QByteArray("99A9B9RESEGVHVX9Y9Z9"));
+    assert(ternary==QByteArray("99A9B9RESEGVHVX9Y9ZZ"));
 
 
     binary=QByteArray::fromHex("9ba06c78552776a596dfe360cc2b5bf644c0f9d343a10e2e71debecd30730d03");
