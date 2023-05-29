9e62fda46d25d027b690c928fb0c4fe516a9922a
EddyGlezz <eddyglezz95@gmail.com>
2023-05-29 09:50:18 +0200

    passed documentation to the headers files only

--- include/encoding/qb1t6.hpp
+++ include/encoding/qb1t6.hpp
@@ -3,12 +3,37 @@
 
 
 namespace qencoding{
+	/*!
+	 * \brief Binary To Ternary Encoding following this
+	 * [TIP](https://github.com/iotaledger/tips/blob/main/tips/TIP-0005/tip-0005.md)
+	 *
+	 */
 	namespace qb1t6{
 
-		
+		/*!
+		 *
+		 * Converts a signed integer value into its corresponding balanced ternary representation in little-endian order
+		 *
+		 */
 		QByteArray intToTrits(const qint8& value);
+		/*!
+		 *
+		 * Converts a QByteArray value into its corresponding balanced ternary representation  
+		 *
+		 */
 		QByteArray get_Trits_from_Bytes(const QByteArray& bytes);
+		/*!
+		 *
+		 * \fn QByteArray get_Trytes_from_Bytes(const QByteArray& bytes)  
+		 * Converts a QByteArray  to the corresponding tryte alphabet string 
+		 *
+		 */
 		QByteArray get_Trytes_from_Bytes(const QByteArray& bytes);
+		/*!
+		 *
+		 * Converts a QByteArray of trits to the corresponding tryte alphabet string 
+		 *
+		 */
 		QByteArray get_Trytes_from_Trits(const QByteArray& trits);
 
 
