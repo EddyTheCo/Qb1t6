f003e9e583cb7c20316cebcd40f4ab16ce9c4eed
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-05-11 16:52:17 +0200

    Develop (#41)
    * install dlls on bin dir (#37)
    
    * use latest deps (#38)
    
    * added .clang-format file (#40)
    
    * C/C++/QML formatted code (#39)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/tip5.cpp
+++ tests/tip5.cpp
@@ -1,24 +1,22 @@
-#include"encoding/qb1t6.hpp"
+#include "encoding/qb1t6.hpp"
 #undef NDEBUG
 #include <assert.h>
 
-
 using namespace qencoding::qb1t6;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    auto binary=QByteArray::fromHex("00");
-    auto ternary=get_Trytes_from_Bytes(binary);
-    assert(ternary==QByteArray("99"));
+    auto binary = QByteArray::fromHex("00");
+    auto ternary = get_Trytes_from_Bytes(binary);
+    assert(ternary == QByteArray("99"));
 
-    binary=QByteArray::fromHex("0001027e7f8081fdfeff");
-    ternary=get_Trytes_from_Bytes(binary);
-    assert(ternary==QByteArray("99A9B9RESEGVHVX9Y9Z9"));
+    binary = QByteArray::fromHex("0001027e7f8081fdfeff");
+    ternary = get_Trytes_from_Bytes(binary);
+    assert(ternary == QByteArray("99A9B9RESEGVHVX9Y9Z9"));
 
+    binary = QByteArray::fromHex("9ba06c78552776a596dfe360cc2b5bf644c0f9d343a10e2e71debecd30730d03");
+    ternary = get_Trytes_from_Bytes(binary);
+    assert(ternary == QByteArray("GWLW9DLDDCLAJDQXBWUZYZODBYPBJCQ9NCQYT9IYMBMWNASBEDTZOYCYUBGDM9C9"));
 
-    binary=QByteArray::fromHex("9ba06c78552776a596dfe360cc2b5bf644c0f9d343a10e2e71debecd30730d03");
-    ternary=get_Trytes_from_Bytes(binary);
-    assert(ternary==QByteArray("GWLW9DLDDCLAJDQXBWUZYZODBYPBJCQ9NCQYT9IYMBMWNASBEDTZOYCYUBGDM9C9"));
-
-	return 0;
+    return 0;
 }
